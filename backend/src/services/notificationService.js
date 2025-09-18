1e1425379806801e8b10a44916147c425b2d08e0
Pedro Henrique <henriquepedro1912@gmail.com>
2025-09-18 09:50:35 -0300

    refatoramento do sistema com integração firebase para notificações

---
+++ backend/src/services/notificationService.js
@@ -0,0 +1,23 @@
+const admin = require("../config/firebase");
+
+async function sendNotification(token, title, body, data = {}) {
+  const message = {
+    token,
+    notification: {
+      title,
+      body,
+    },
+    data, // dados extras opcionais
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log("Notificação enviada:", response);
+    return response;
+  } catch (error) {
+    console.error("Erro ao enviar notificação:", error);
+    throw error;
+  }
+}
+
+module.exports = { sendNotification };
